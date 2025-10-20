32ee25511d0f9d91ab3ef1c29bf8b897c34350b3
ST10494032 <ST10494032@vcconnect.edu.za>
2025-10-20 17:05:24 +0200

    Added all files needed

--- App.tsx
+++ App.tsx
@@ -1,32 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import AddItemScreen from "./screens/AddItemScreen";
-import { MenuItem } from "./types/MenuItem";
-
-export type RootStackParamList = {
-  Home: { menuItems: MenuItem[] } | undefined;
-  AddItem: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Chef Christoffel’s Menu", headerStyle: { backgroundColor: "#B8860B" }, headerTintColor: "#fff" }}
-        />
-        <Stack.Screen
-          name="AddItem"
-          component={AddItemScreen}
-          options={{ title: "Add New Dish", headerStyle: { backgroundColor: "#B8860B" }, headerTintColor: "#fff" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
